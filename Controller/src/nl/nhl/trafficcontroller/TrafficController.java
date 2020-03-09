f7349580ab3217bce9e2ed2d63acd8044142f168
BaukePostma <bauke.j.p@hotmail.com>
2020-03-09 20:23:03 +0100

    V 0.1 communication
    Working Node.JS server , working Unity client

--- Controller/src/nl/nhl/trafficcontroller/TrafficController.java
+++ Controller/src/nl/nhl/trafficcontroller/TrafficController.java
@@ -1,7 +1,28 @@
 package nl.nhl.trafficcontroller;
 
+import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
+
 public class TrafficController {
     public static void main(String[] args) {
         System.out.println("Hallo Allemaal");
+        WebsocketConnection connection = new WebsocketConnection();
+        while (true) {
+            String[] helloWorld = new String[]{"Hello", "World"};
+        }
+
+/*
+
+try{
+         //   connection.TestConnection();
+            connection.TestSendJSON();
+        }
+        catch(NoSuchAlgorithmException | IOException e){
+
+        }
+
+ */
+
+
     }
 }
---
+++ socketiocontroller/index.js
@@ -0,0 +1,17 @@
+const WebSocket = require('ws');
+
+const wss = new WebSocket.Server({ port: 8080 });
+
+wss.on('connection', function connection(ws) {
+
+console.log("Connection made");
+
+    ws.on('message', function incoming(data) {
+        wss.clients.forEach(function each(client) {
+            if (client.readyState === WebSocket.OPEN) {
+                client.send('Oh green world');
+                console.log(data);
+            }
+        });
+    });
+});
